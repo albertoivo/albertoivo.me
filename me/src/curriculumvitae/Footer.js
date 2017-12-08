b0d3b5887c0619f13f41f55b7e3259feeb8dc9a9
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-12-08 10:59:15 -0200

    adding linkedin social media

--- me/src/curriculumvitae/Footer.js
+++ me/src/curriculumvitae/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { FaFacebookSquare, FaInstagram } from 'react-icons/lib/fa'
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaLinkedinSquare
+} from 'react-icons/lib/fa'
 
 const Footer = () => (
   <div>
     <p>Find me on social media</p>
-    <FaFacebookSquare />
-    <FaInstagram />
+    <FaFacebookSquare /> <FaInstagram /> <FaLinkedinSquare />
     <p>
       Powered by{' '}
       <a href="http://albertoivo.me" target="_blank" rel="noopener noreferrer">
