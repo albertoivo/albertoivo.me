a3958f7e7d3cc0fd9ee3f8f5c37d12e979853dc9
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-12-11 11:02:39 -0200

    adding react-router-dom

--- me/src/App.js
+++ me/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Principal from './Principal'
 import Curriculum from './curriculumvitae/Curriculum'
+import Portfolio from './portfolio/Portfolio'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Curriculum />
+        <Switch>
+          <Route exact path="/" component={Principal} />
+          <Route exact path="/curriculum" component={Curriculum} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Switch>
       </div>
     )
   }
