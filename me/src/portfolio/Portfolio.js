e284e195b5456561c7ae0c2ef7f0291a5153a3f3
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-12-13 14:46:07 -0200

    Seamless Responsive Photo Grid

--- me/src/portfolio/Portfolio.js
+++ me/src/portfolio/Portfolio.js
@@ -6,16 +6,16 @@ const Portfolio = () => (
   <div>
     <div className="port-container">
       <div className="port-menu">oi</div>
-      <div className="port-main">
-        <div style={{ backgroundColor: 'red' }}>oi</div>
-        <div style={{ backgroundColor: 'red' }}>oi</div>
-        <div style={{ backgroundColor: 'red' }}>oi</div>
-        <div style={{ backgroundColor: 'red' }}>oi</div>
-        <div style={{ backgroundColor: 'red' }}>oi</div>
-        <div style={{ backgroundColor: 'red' }}>oi</div>
-        <div style={{ backgroundColor: 'red' }}>oi</div>
-        <div style={{ backgroundColor: 'red' }}>oi</div>
-        <div style={{ backgroundColor: 'red' }}>oi</div>
+      <div id="photos">
+        <img src="https://placekitten.com/234/234" alt="1" />
+        <img src="https://placekitten.com/494/232" alt="2" />
+        <img src="https://placekitten.com/434/132" alt="3" />
+        <img src="https://placekitten.com/404/332" alt="4" />
+        <img src="https://placekitten.com/224/432" alt="5" />
+        <img src="https://placekitten.com/344/432" alt="6" />
+        <img src="https://placekitten.com/434/432" alt="7" />
+        <img src="https://placekitten.com/438/332" alt="8" />
+        <img src="https://placekitten.com/234/432" alt="9" />
       </div>
     </div>
     <Footer css={'port-footer'} />
