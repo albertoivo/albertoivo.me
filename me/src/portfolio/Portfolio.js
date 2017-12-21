a96aa18f8b9035ac19d6381c43989fd74151072f
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-12-21 10:30:46 -0200

    portfolio is now using react-images LightBox

--- me/src/portfolio/Portfolio.js
+++ me/src/portfolio/Portfolio.js
@@ -1,106 +1,131 @@
 import React from 'react'
 import Footer from '../principal/Footer'
+import Lightbox from 'react-images'
 import './portfolio.css'
 
-const Portfolio = () => (
-  <div>
-    <div className="port-container">
-      <div className="port-menu">
-        <div id="categories">
-          <ul>
-            <li>Nature</li>
-            <li>Portrait</li>
-            <li>Northern Lights</li>
-            <li>Selective Color</li>
-          </ul>
-        </div>
-      </div>
-      <div id="photos">
-        <img src="https://placekitten.com/334/234" alt="1" />
-        <img src="https://placekitten.com/294/232" alt="2" />
-        <img src="https://placekitten.com/534/132" alt="3" />
-        <img src="https://placekitten.com/304/332" alt="4" />
-        <img src="https://placekitten.com/224/332" alt="5" />
-        <img src="https://placekitten.com/354/232" alt="6" />
-        <img src="https://placekitten.com/484/432" alt="7" />
-        <img src="https://placekitten.com/438/332" alt="8" />
-        <img src="https://placekitten.com/294/432" alt="9" />
-        <img src="https://placekitten.com/264/234" alt="1" />
-        <img src="https://placekitten.com/494/332" alt="2" />
-        <img src="https://placekitten.com/434/132" alt="3" />
-        <img src="https://placekitten.com/404/332" alt="4" />
-        <img src="https://placekitten.com/324/432" alt="5" />
-        <img src="https://placekitten.com/344/232" alt="6" />
-        <img src="https://placekitten.com/434/432" alt="7" />
-        <img src="https://placekitten.com/438/232" alt="8" />
-        <img src="https://placekitten.com/234/432" alt="9" />
-        <img src="https://placekitten.com/234/234" alt="1" />
-        <img src="https://placekitten.com/394/232" alt="2" />
-        <img src="https://placekitten.com/334/232" alt="3" />
-        <img src="https://placekitten.com/404/332" alt="4" />
-        <img src="https://placekitten.com/224/432" alt="5" />
-        <img src="https://placekitten.com/344/432" alt="6" />
-        <img src="https://placekitten.com/434/432" alt="7" />
-        <img src="https://placekitten.com/438/332" alt="8" />
-        <img src="https://placekitten.com/234/432" alt="9" />
-        <img src="https://placekitten.com/234/234" alt="1" />
-        <img src="https://placekitten.com/494/232" alt="2" />
-        <img src="https://placekitten.com/434/132" alt="3" />
-        <img src="https://placekitten.com/404/332" alt="4" />
-        <img src="https://placekitten.com/224/432" alt="5" />
-        <img src="https://placekitten.com/454/432" alt="6" />
-        <img src="https://placekitten.com/384/432" alt="7" />
-        <img src="https://placekitten.com/238/332" alt="8" />
-        <img src="https://placekitten.com/294/332" alt="9" />
-        <img src="https://placekitten.com/264/334" alt="1" />
-        <img src="https://placekitten.com/494/432" alt="2" />
-        <img src="https://placekitten.com/434/132" alt="3" />
-        <img src="https://placekitten.com/404/332" alt="4" />
-        <img src="https://placekitten.com/324/432" alt="5" />
-        <img src="https://placekitten.com/444/232" alt="6" />
-        <img src="https://placekitten.com/334/432" alt="7" />
-        <img src="https://placekitten.com/238/232" alt="8" />
-        <img src="https://placekitten.com/234/432" alt="9" />
-        <img src="https://placekitten.com/294/234" alt="1" />
-        <img src="https://placekitten.com/324/232" alt="2" />
-        <img src="https://placekitten.com/334/292" alt="3" />
-        <img src="https://placekitten.com/494/332" alt="4" />
-        <img src="https://placekitten.com/284/412" alt="5" />
-        <img src="https://placekitten.com/344/432" alt="6" />
-        <img src="https://placekitten.com/434/432" alt="7" />
-        <img src="https://placekitten.com/438/332" alt="8" />
-        <img src="https://placekitten.com/234/432" alt="9" />
-        <img src="https://placekitten.com/234/234" alt="1" />
-        <img src="https://placekitten.com/494/232" alt="2" />
-        <img src="https://placekitten.com/434/132" alt="3" />
-        <img src="https://placekitten.com/404/332" alt="4" />
-        <img src="https://placekitten.com/224/432" alt="5" />
-        <img src="https://placekitten.com/354/432" alt="6" />
-        <img src="https://placekitten.com/484/432" alt="7" />
-        <img src="https://placekitten.com/438/332" alt="8" />
-        <img src="https://placekitten.com/294/432" alt="9" />
-        <img src="https://placekitten.com/264/234" alt="1" />
-        <img src="https://placekitten.com/494/332" alt="2" />
-        <img src="https://placekitten.com/434/132" alt="3" />
-        <img src="https://placekitten.com/404/332" alt="4" />
-        <img src="https://placekitten.com/324/432" alt="5" />
-        <img src="https://placekitten.com/344/232" alt="6" />
-        <img src="https://placekitten.com/434/432" alt="7" />
-        <img src="https://placekitten.com/438/232" alt="8" />
-        <img src="https://placekitten.com/234/432" alt="9" />
-        <img src="https://placekitten.com/234/234" alt="1" />
-        <img src="https://placekitten.com/394/232" alt="2" />
-        <img src="https://placekitten.com/334/232" alt="3" />
-        <img src="https://placekitten.com/404/332" alt="4" />
-        <img src="https://placekitten.com/224/432" alt="5" />
-        <img src="https://placekitten.com/344/432" alt="6" />
-        <img src="https://placekitten.com/434/432" alt="7" />
-        <img src="https://placekitten.com/438/332" alt="8" />
-        <img src="https://placekitten.com/234/432" alt="9" />
-      </div>
-    </div>
-    <Footer css={'port-footer'} />
-  </div>
-)
+class Portfolio extends React.Component {
+  constructor() {
+    super()
 
+    this.state = {
+      lightboxIsOpen: false,
+      currentImage: 0
+    }
+
+    this.closeLightbox = this.closeLightbox.bind(this)
+    this.gotoNext = this.gotoNext.bind(this)
+    this.gotoPrevious = this.gotoPrevious.bind(this)
+    this.gotoImage = this.gotoImage.bind(this)
+
+    this.openLightbox = this.openLightbox.bind(this)
+  }
+  openLightbox(index, event) {
+    event.preventDefault()
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true
+    })
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false
+    })
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1
+    })
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1
+    })
+  }
+  gotoImage(index) {
+    this.setState({
+      currentImage: index
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="port-container">
+          <div className="port-menu">
+            <div id="categories">
+              <ul>
+                <li>Nature</li>
+                <li>Portrait</li>
+                <li>Northern Lights</li>
+                <li>Selective Color</li>
+              </ul>
+            </div>
+          </div>
+          <div id="photos">
+            <a
+              href="https://placekitten.com/334/234"
+              onClick={e => this.openLightbox(0, e)}
+            >
+              <img src="https://placekitten.com/334/234" />
+            </a>
+
+            <a
+              href="https://placekitten.com/294/232"
+              onClick={e => this.openLightbox(1, e)}
+            >
+              <img src="https://placekitten.com/294/232" />
+            </a>
+
+            <a
+              href="https://placekitten.com/534/132"
+              onClick={e => this.openLightbox(2, e)}
+            >
+              <img src="https://placekitten.com/534/132" />
+            </a>
+
+            <a
+              href="https://placekitten.com/334/234"
+              onClick={e => this.openLightbox(3, e)}
+            >
+              <img src="https://placekitten.com/334/234" />
+            </a>
+
+            <a
+              href="https://placekitten.com/294/232"
+              onClick={e => this.openLightbox(4, e)}
+            >
+              <img src="https://placekitten.com/294/232" />
+            </a>
+
+            <a
+              href="https://placekitten.com/534/132"
+              onClick={e => this.openLightbox(5, e)}
+            >
+              <img src="https://placekitten.com/534/132" />
+            </a>
+
+            <Lightbox
+              images={[
+                { src: 'https://placekitten.com/334/234' },
+                { src: 'https://placekitten.com/294/232' },
+                { src: 'https://placekitten.com/534/132' },
+                { src: 'https://placekitten.com/334/234' },
+                { src: 'https://placekitten.com/294/232' },
+                { src: 'https://placekitten.com/534/132' }
+              ]}
+              currentImage={this.state.currentImage}
+              isOpen={this.state.lightboxIsOpen}
+              onClickNext={this.gotoNext}
+              onClickPrev={this.gotoPrevious}
+              onClickThumbnail={this.gotoImage}
+              onClose={this.closeLightbox}
+            />
+          </div>
+        </div>
+        <Footer css={'port-footer'} />
+      </div>
+    )
+  }
+}
 export default Portfolio
